c60c39215989f36ef73d1c511151f4e37c913467
Cody Hsieh <cohsieh@berkeley.edu>
2018-04-10 00:56:21 -0700

    fix top k with tf.where

--- scripts/retrain.py
+++ scripts/retrain.py
@@ -827,9 +827,6 @@ def add_evaluation_step(result_tensor, ground_truth_tensor):
       correct_prediction = tf.equal(prediction, tf.argmax(ground_truth_tensor, 1))
       # print(tf.argmax(ground_truth_tensor, 1, output_type=tf.int32))
       correct_prediction_top_5 = tf.equal(tf.cast(tf.gather(top_k_vals, 0, axis=1), tf.int32), tf.argmax(ground_truth_tensor, 1, output_type=tf.int32))
-      
-      def assign_top_k(is_equal):
-        correct_prediction_top_5 = is_equal
       for i in range(1, 5):
       #   print(tf.shape(tf.gather(top_k_inicides, i, axis=1)))
       #   print(tf.shape(tf.argmax(ground_truth_tensor, 1, output_type=tf.int32)))
